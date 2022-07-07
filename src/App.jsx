4137c7a74fd0fb6791ab407bbea6564535828c59
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-07-06 23:19:00 -0300

    chore: use footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Counter from "./components/Counter/Counter";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Footer text="Made by Patricia Magalhães - 2022" />
     </div>
   );
 }
